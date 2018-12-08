53fbff01fe76d09f7944d648da9b56162dd57760
Luciano Croce <19188077+luciano-croce@users.noreply.github.com>
2018-12-08 17:07:29 +0100

    Error in const plugin_version
    - Error in const plugin_version: chabge -> const plugin_version = 1.0; to -> const plugin_version = 1.2;
    
    Also same error in wordpress.org plugin repository: change -> const plugin_version = 1.0; -> const plugin_version = 1.1;

--- classic-editor.php
+++ classic-editor.php
@@ -28,7 +28,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 if ( ! class_exists( 'Classic_Editor' ) ) :
 class Classic_Editor {
-	const plugin_version = 1.0;
+	const plugin_version = 1.2;
 	private static $settings;
 	private static $supported_post_types = array();
 
