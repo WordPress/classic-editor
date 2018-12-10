1ef1853f99d866f11a7b20d57bab90e2a88e2b0e
Luciano Croce <19188077+luciano-croce@users.noreply.github.com>
2018-12-10 12:27:13 +0100

    block-editor-plugin.js -- reduce the script size
    - Deleting all blank lines to reduce the script size when it is enabled
    - Consider the possibility of creating a minifiled file
    
    block-editor-plugin.js
    block-editor-plugin.min.js
    
    IMHO a smaller size is always better, and speed-up loading.

--- js/block-editor-plugin.js
+++ js/block-editor-plugin.js
@@ -4,12 +4,10 @@
 	const { PluginMoreMenuItem } = wp.editPost;
 	const { addQueryArgs } = wp.url;
 	const { registerPlugin } = wp.plugins;
-
 	registerPlugin( 'classic-editor-add-submenu', {
 		render() {
 			const url = addQueryArgs( document.location.href, { 'classic-editor': null } );
 			const linkText = get( window, [ 'classicEditorPluginL10n', 'linkText' ] ) || 'Switch to Classic Editor';
-
 			return createElement(
 				PluginMoreMenuItem,
 				{
