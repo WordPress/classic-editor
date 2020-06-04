5cc5c8ad291b15ed2e8df18ac74d6ca02f603dea
Copons <Copons@users.noreply.github.com>
2020-06-04 18:35:10 +0100

    Add permissions check

--- class-classic-editor-endpoint.php
+++ class-classic-editor-endpoint.php
@@ -9,6 +9,7 @@ class Classic_Editor_Endpoint extends WP_REST_Controller {
 			array(
 				'methods'             => WP_REST_Server::READABLE,
 				'callback'            => array( $this, 'get_user_settings' ),
+				'permission_callback' => array( $this, 'get_user_settings_permissions_check' ),
 				'args'                => array(
 				'user_id'     => array(
 					'description'       => 'ID of user to get settings for.',
@@ -20,7 +21,7 @@ class Classic_Editor_Endpoint extends WP_REST_Controller {
   	}
  
 	/**
-	* Get classic editor plugin settings for a given user id
+	* Get Classic Editor plugin settings for a given user id
 	* 
 	* @param WP_REST_Request $request Full data about the request.
 	* @return WP_Error|WP_REST_Response
@@ -35,5 +36,39 @@ class Classic_Editor_Endpoint extends WP_REST_Controller {
 		);
 
 		return new WP_REST_Response( $response, 200 );
+	}
+
+	/**
+	* Check if a given request has access to the user's Classic Editor plugin settings.
+	* 
+	* @param WP_REST_Request $request Full data about the request.
+	* @return true|WP_Error
+	*/
+	public function get_user_settings_permissions_check( $request ) {
+		$error = new WP_Error(
+			'rest_user_invalid_id',
+			__( 'Invalid user ID.' ),
+			array( 'status' => 404 )
+		);
+
+		$user_id = $request['user_id'];
+		$user = get_userdata( (int) $id );
+		if ( empty( $user ) || ! $user->exists() ) {
+			return $error;
+		}
+
+		if ( is_multisite() && ! is_user_member_of_blog( $user->ID ) ) {
+			return $error;
+		}
+
+		if ( get_current_user_id() === $user->ID ) {
+			return true;
+		}
+
+		if ( current_user_can( 'list_users' ) ) {
+			return true;
+		}
+
+		return $error;
 	}
 }
