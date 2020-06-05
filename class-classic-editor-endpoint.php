5ab1cf1eb18001f8dd04821b9a1b868057490c14
Glen Davies <glen.davies@a8c.com>
2020-06-05 15:55:22 +1200

    Add a second endpoint to get post related settings

--- class-classic-editor-endpoint.php
+++ class-classic-editor-endpoint.php
@@ -5,10 +5,17 @@ class Classic_Editor_Endpoint extends WP_REST_Controller {
 		$version = '1';
 		$namespace = 'classic-editor/v' . $version;
 
-		register_rest_route( $namespace, '/settings', array(
+		register_rest_route( $namespace, '/user-settings', array(
 			array(
 				'methods'             => WP_REST_Server::READABLE,
 				'callback'            => array( $this, 'get_user_settings' ),
+				'permission_callback' => array( $this, 'get_user_settings_permissions_check' ),
+			) ) );
+
+		register_rest_route( $namespace, '/post-settings/(?P<id>[\d]+)', array(
+			array(
+				'methods'             => WP_REST_Server::READABLE,
+				'callback'            => array( $this, 'get_post_settings' ),
 				'permission_callback' => array( $this, 'get_user_settings_permissions_check' ),
 			) ) );
   	}
@@ -16,20 +23,18 @@ class Classic_Editor_Endpoint extends WP_REST_Controller {
 	/**
 	* Check if a given request has access to the user's Classic Editor plugin settings.
 	* 
-	* @param WP_REST_Request $request Full data about the request.
 	* @return true|WP_Error
 	*/
-	public function get_user_settings_permissions_check( $request ) {
-		return get_current_user_id() > 0;
+	public function get_user_settings_permissions_check() {
+		return current_user_can( 'edit_posts' );
 	}
 
 	/**
-	* Get Classic Editor plugin settings for a given user id
+	* Get Classic Editor plugin settings for current user
 	* 
-	* @param WP_REST_Request $request Full data about the request.
 	* @return WP_Error|WP_REST_Response
 	*/
-	public function get_user_settings( $request ) {
+	public function get_user_settings() {
 		$settings = Classic_Editor::get_settings();
 
 		$response = array(
@@ -38,5 +43,27 @@ class Classic_Editor_Endpoint extends WP_REST_Controller {
 		);
 
 		return new WP_REST_Response( $response, 200 );
+	}
+
+	/**
+	* Get Classic Editor plugin settings for a specific post
+	* 
+	* @param WP_REST_Request $request Full data about the request.
+	* @return WP_Error|WP_REST_Response
+	*/
+	public function get_post_settings( $request ) {
+		$post_id = $request['id'];
+		$settings = Classic_Editor::get_settings();
+
+		$use_block_editor = 'block' === $settings['editor'];
+		$post = get_post( $post_id );
+
+		if ( ! $post ) {
+			return new WP_REST_Response( array( 'selected_editor' => $settings['editor'] ), 200 );
+		}
+
+		$editor = Classic_Editor::choose_editor( $use_block_editor, $post ) ? 'block' : 'classic';
+
+		return new WP_REST_Response( array( 'selected_editor' => $editor ), 200 );
 	}
 }
